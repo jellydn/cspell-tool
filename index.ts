6948335ce518d345ff4978309821df158e7350f9
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-16 09:33:09 +0800

    feat: add consola to better log

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-import { $ } from "zx";
 import { exec } from "child_process";
+import consola from "consola";
+import { $ } from "zx";
 
 import { writeFile } from "./lib";
 
@@ -9,12 +10,12 @@ let isInstalled = false;
 try {
   await $`cspell --version`;
   isInstalled = true;
-} catch (error) {
-  console.error(
+} catch {
+  consola.warn(
     "cSpell is not installed. Please install with your package manager.",
   );
   // Print hint to console with cmd: npm install -g cspell@latest
-  console.info("Hint: npm install -g cspell@latest");
+  consola.info("Hint: npm install -g cspell@latest");
 }
 
 // Extract project name from current directory
@@ -44,14 +45,17 @@ writeFile(`./${projectName}.txt`, "");
 writeFile("./cspell.json", JSON.stringify(cSpellContent, null, 2));
 
 // TODO: Support other file types
+const fileTypes = ["md", "ts", "json", "lua"];
+const cspellCmd = isInstalled ? "cspell" : "npx cspell";
+
 // Run cspell on Markdown files to get unknown words
-const cmd = `${
-  isInstalled ? "" : "npx "
-}cspell --words-only --unique --no-progress --show-context "**/**/*.md" "**/**/*.ts" "**/**/*.json"`;
+const cmd = `${cspellCmd} --words-only --unique --no-progress --show-context ${fileTypes
+  .map((fileType) => `"**/**/*.${fileType}"`)
+  .join(" ")}`;
 const unknownWords = await new Promise<string[]>((resolve) => {
   exec(cmd, (error: any, stdout: string) => {
     if (error) {
-      console.error(`Error running cspell: ${error}`);
+      consola.error(error);
     }
 
     const words = stdout ? stdout.split("\n").filter((word: any) => word) : [];
@@ -59,9 +63,9 @@ const unknownWords = await new Promise<string[]>((resolve) => {
   });
 });
 
-console.log(`Found ${unknownWords.length} unknown words.`);
+consola.log(`Found ${unknownWords.length} unknown words.`);
 
 // Save unknown words in project-name.txt
 writeFile(`./${projectName}.txt`, unknownWords.join("\n"));
-console.log("cSpell setup completed.");
+consola.success("cSpell setup completed.");
 process.exit(0);
